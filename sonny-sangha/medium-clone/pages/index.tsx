786222f3437c36ee235e958d88b532b457c3f27f
EliabLM <eliablopez@hotmail.com>
2022-09-13 19:35:18 -0500

    Add Hero section

--- sonny-sangha/medium-clone/pages/index.tsx
+++ sonny-sangha/medium-clone/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -6,12 +7,36 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium blog</title>
         {/* <link rel="icon" href="/public/favicon.ico" /> */}
       </Head>
       <Header />
+
+      {/* Hero section */}
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 ">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4">
+              Medium
+            </span>{" "}
+            is a place to write, read, and connect
+          </h1>
+          <h2>
+            It&apos;s easy and free to post your thinking on any topic and
+            connect with millions of readers.
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-32 lg:h-52 px-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Medium_icon.svg/1200px-Medium_icon.svg.png"
+          alt="logo"
+        />
+      </div>
+
+      {/* Posts */}
     </div>
   );
 };
