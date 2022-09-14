c90db4b4a4cffe268436f0e782ce19115e783b16
EliabLM <eliablopez@hotmail.com>
2022-09-13 19:17:24 -0500

    Add Header componente and Sanity configuration

--- sonny-sangha/medium-clone/pages/index.tsx
+++ sonny-sangha/medium-clone/pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello medium clone</h1>
+      <Head>
+        <title>Medium blog</title>
+        {/* <link rel="icon" href="/public/favicon.ico" /> */}
+      </Head>
+      <Header />
     </div>
   );
 };
