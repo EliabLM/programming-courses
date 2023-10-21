71e8fc56b67504c7b6741e2e50be9f487c4de067
Eliab Lopez Murillo <elopez@heinsohn.com.co>
2023-10-21 11:59:40 -0500

    Fundamentos de CSharp y Linq

---
+++ csharp-linq/console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+
+var frutas = new string[] {
+    "Sandia", "Fresa", "Mango", "Mango de azúcar", "Mango Tomy"
+};
+
+var MangoList = frutas.Where(item => item.StartsWith("Mango")).ToList();
+MangoList.ForEach(item => Console.WriteLine(item));
+
+Console.WriteLine(MangoList);
+
+
+
