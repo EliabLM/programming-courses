8210bacd546e919f0d0bf80ff5e23f51c69d6ec4
Eliab Lopez Murillo <elopez@heinsohn.com.co>
2024-01-12 17:18:43 -0500

    Agrega DB SQL Server y configura appsettings

--- csharp-ef/Program.cs
+++ csharp-ef/Program.cs
@@ -1,6 +1,20 @@
+using csharpef;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("conexionTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    await dbContext.Database.EnsureCreatedAsync();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
